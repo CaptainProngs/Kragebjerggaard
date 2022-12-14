cd521a215a7d1ddd9287d29919a9da4d705fa0dc
holms <anna61c9@edu.zealand.dk>
2022-12-14 13:36:42 +0100

    Footer + billeder.

--- index.php
+++ index.php
@@ -51,6 +51,48 @@
 
 
 
+<a class="nav-link d-lg-none p-0 m-0" href="#"><img src="images/footerGrafikS.png" alt="Map"></a>
+<a class="container-fluid nav-link d-none d-lg-block p-0 m-0" href="#"><img src="images/footerGrafikL.png" alt="largemap"></a>
+
+<footer class="navbar navbar-expand-lg bg-navfarve fixed-bottom">
+    <div class="container-fluid" style="max-height: 200px>
+
+            <div class="row justify-content-center d-flex">
+                <div class="col col-lg-4">
+                    <a class="nav-link active p-2 px-5" style="font-size: x-large" style="font-family: 'Sans Mateo 2 Semi'" href="">Info</a>
+                    <a class="nav-link p-2 px-5" style="font-size: large" href="#">Gården</a>
+                    <a class="nav-link p-2 px-5" style="font-size: large" href="#">Click & collect</a>
+                    <a class="nav-link p-2 px-5" style="font-size: large" href="#">Handelsbetingelser</a>
+                    <a class="nav-link p-2 px-5" style="font-size: large" href="#">Persondatapolitik</a>
+                    <a class="nav-link p-2 px-5" style="font-size: large" href="#">Fødevarestyrelsen</a>
+                    <a class="nav-link p-2 px-5" style="font-size: large" href="#">Cookies</a>
+                    <a class="nav-link p-2 px-5" style="font-size: large" href="#">Bæredygtighed</a>
+                </div>
+                <div class="col col-lg-4">
+                    <a class="nav-link active p-2 px-5" style="font-size: x-large"
+                       style="font-family: 'Sans Mateo 2 Semi'" href="">Åbningstider</a>
+                    <a class="nav-link p-2 px-5" style="font-size: large" href="#">Mandag - fredag 11.00 -19:00</a>
+                    <a class="nav-link p-2 px-5" style="font-size: large" href="#">Søndag & helligedage 12.00 -
+                        16.00</a>
+                </div>
+                <div class="col col-lg-4">
+                    <a class="nav-link px-4"
+                       href="https://www.google.com/maps/place/Kragebjergg%C3%A5rd/@55.5481506,11.6582068,17z/data=!3m1!4b1!4m5!3m4!1s0x46528422abd36d6d:0x6559ba81305fb08b!8m2!3d55.5481426!4d11.6603878">
+                        <img style="max-height: 40px" src="images/map.png" alt="cart"></a>
+                    <a class="nav-link" href="#"><img src="images/Facebook.png" alt="Facebook"></a>
+                    <a class="nav-link" href="#"><img src="images/Instagram.png" alt="instgram"></a>
+                </div>
+            </div>
+
+            <div class="row justify-content-center d-flex rettigheder">
+                <div class="col">
+                    <img src="images/rettigheder.png" alt="copyrights">
+                </div>
+            </div>
+    </div>
+</footer>
+
+
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
