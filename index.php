b20efcf125053b88b979044032d2f96949e39506
holms <anna61c9@edu.zealand.dk>
2022-12-14 14:26:11 +0100

    navbar rettelser.

--- index.php
+++ index.php
@@ -61,22 +61,21 @@
 <a class="nav-link d-lg-none p-0 m-0" href="#"><img src="images/footerGrafikS.png" alt="Map"></a>
 <a class="container-fluid nav-link d-none d-lg-block p-0 m-0" href="#"><img src="images/footerGrafikL.png" alt="largemap"></a>
 
-    <footer class="navbar container-fluid bg-navfarve justify-content-center" style="max-height: 200px>
+    <footer class="footer container-fluid bg-navfarve justify-content-center" style="max-height: 200px>
 
         <div class="row d-flex">
             <div class="col col-lg-4">
-                <a class="nav-link active p-2 px-5" style="font-size: x-large" style="font-family: 'Sans Mateo 2 Semi'" href="">Info</a>
-                <a class="nav-link p-2 px-5" style="font-size: large" href="#">Gården</a>
-                <a class="nav-link p-2 px-5" style="font-size: large" href="#">Click & collect</a>
-                <a class="nav-link p-2 px-5" style="font-size: large" href="#">Fødevarestyrelsen</a>
-                <a class="nav-link p-2 px-5" style="font-size: large" href="#">Cookies</a>
-                <a class="nav-link p-2 px-5" style="font-size: large" href="#">Bæredygtighed</a>
+                <a class="nav-link active p-2 px-5" style="font-size: x-large" href="">Info</a>
+                <a class="link p-2 px-5" style="font-size: large" href="#">Gården</a>
+                <a class="link p-2 px-5" style="font-size: large" href="#">Click & collect</a>
+                <a class="link p-2 px-5" style="font-size: large" href="#">Fødevarestyrelsen</a>
+                <a class="link p-2 px-5" style="font-size: large" href="#">Cookies</a>
+                <a class="link p-2 px-5" style="font-size: large" href="#">Bæredygtighed</a>
             </div>
             <div class="col col-lg-4">
-                <a class="nav-link active p-2 px-5" style="font-size: x-large"
-                   style="font-family: 'Sans Mateo 2'" href="">Åbningstider</a>
-                <a class="nav-link p-2 px-5" style="font-size: large" href="#">Mandag - fredag 11.00 -19:00</a>
-                <a class="nav-link p-2 px-5" style="font-size: large" href="#">Søndag & helligedage 12.00 -
+                <a class="nav-link active p-2 px-5" style="font-size: x-large" href="">Åbningstider</a>
+                <a class="link p-2 px-5" style="font-size: large" href="#">Mandag - fredag 11.00 -19:00</a>
+                <a class="link p-2 px-5" style="font-size: large" href="#">Søndag & helligedage 12.00 -
                     16.00</a>
             </div>
             <div class="col col-lg-4">
