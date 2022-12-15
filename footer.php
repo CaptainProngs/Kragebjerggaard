fff6bf4a185d4c8e9ee21b08929bd51d2c72b509
holms <anna61c9@edu.zealand.dk>
2022-12-15 11:40:43 +0100

    footer include test.

---
+++ footer.php
@@ -0,0 +1,48 @@
+<?php
+echo "
+
+
+<a class="nav-link d-lg-none p-0 m-0" href="#"><img src="images/footerGrafikS.png" alt="Map"></a>
+<a class="container-fluid nav-link d-none d-lg-block p-0 m-0" href="#"><img src="images/footerGrafikL.png" alt="largemap"></a>
+
+<footer class="footer container-fluid bg-navfarve justify-content-center" style="min-height: 200px">
+
+    <div class="row pt-5 d-flex justify-content-around">
+        <div class="col col-lg-3">
+            <a class="nav-link active" style="font-size: x-large" href="">Info</a>
+            <a class="link row p-2" style="font-size: large" href="#">Gården</a>
+            <a class="link row p-2" style="font-size: large" href="#">Click & collect</a>
+            <a class="link row p-2" style="font-size: large" href="#">Fødevarestyrelsen</a>
+            <a class="link row p-2" style="font-size: large" href="#">Cookies</a>
+            <a class="link row p-2" style="font-size: large" href="#">Bæredygtighed</a>
+        </div>
+        <div class="col col-lg-3">
+            <a class="nav-link active" style="font-size: x-large" href="">Åbningstider</a>
+            <a class="link row p-2" style="font-size: large" href="#">Mandag - fredag 11.00 - 19:00</a>
+            <a class="link row p-2" style="font-size: large" href="#">Søndag & helligedage 12.00 - 16.00</a>
+        </div>
+        <div class="col col-lg-3">
+            <a class="nav-link px-4"
+               href="https://www.google.com/maps/place/Kragebjergg%C3%A5rd/@55.5481506,11.6582068,17z/data=!3m1!4b1!4m5!3m4!1s0x46528422abd36d6d:0x6559ba81305fb08b!8m2!3d55.5481426!4d11.6603878">
+                <img style="min-width: 100px" src="images/map.png" alt="cart"></a>
+            <div class="row justify-content-center">
+                <a class="nav-link col-2" href="#"><img src="images/Facebook.png" alt="Facebook"></a>
+                <a class="nav-link col-3" href="#"><img src="images/Instagram.png" alt="instagram"></a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<div class="container-fluid bg-navfarve" style="height: 8rem">
+    <div class="row justify-content-center d-flex rettigheder">
+        <div class="col-8 col-lg-4">
+            <img src="images/rettigheder.png" alt="copyrights">
+        </div>
+    </div>
+</div>
+
+
+
+";
+?>
--- index.php
+++ index.php
@@ -95,7 +95,7 @@
         </div>
     </div>
 </div>
-
+<?php include 'footer.php';?>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
