b607cd75f932a2dc03c7343745165cb667e0e220
Charlie <camille@camillekirstine.com>
2022-12-16 20:54:49 +0100

    added graphics for about page

--- about.php
+++ about.php
@@ -1,11 +1,9 @@
-<html lang="da">
-
-
+<!DOCTYPE html>
+<html lang="da" xmlns="http://www.w3.org/1999/html">
 <head>
-
     <meta charset="utf-8">
 
-    <title>Sigende titel</title>
+    <title>Kragebjerggaard - Om os</title>
 
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
@@ -15,14 +13,48 @@
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link href="https://fonts.cdnfonts.com/css/sans-mateo" rel="stylesheet">
+    <script src="https://cdn.tiny.cloud/1/ys9ivv1x233eg2mrq0d4prcpenkssm67kjeagomz2eix9ubu/tinymce/6/tinymce.min.js"
+            referrerpolicy="origin"></script>
 </head>
 
 <body>
 
 <?php include "nav.php"?>
 
+<section class="aboutus">
+    <div class="aboutus-img-cow" >
+        <img src="images/cutecowaboutus.png">
+
+        <div class="text-end aboutus-title">
+           <p>Om os</p>
+        </div>
+
+        <div class="aboutus-img-footer" >
+            <img src="images/footergraphicwhitePC.png">
+        </div>
+
+    </div>
+</section>
 
 
+
+<main>
+    <div class="container-fluid bg-oliveGreen mt-5">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-6 col-lg-3"></div>
+                <div class="col-6 col-lg-3"></div>
+                <div class="col-6 col-lg-3"></div>
+                <div class="col-6 col-lg-3"></div>
+            </div>
+        </div>
+    </div>
+
+</main>
 
 <?php include "footer.php" ?>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-
+    <link href="https://fonts.cdnfonts.com/css/sans-mateo" rel="stylesheet">
     <script src="https://cdn.tiny.cloud/1/ys9ivv1x233eg2mrq0d4prcpenkssm67kjeagomz2eix9ubu/tinymce/6/tinymce.min.js"
             referrerpolicy="origin"></script>
 </head>
