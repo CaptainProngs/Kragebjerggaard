941c2aec118eda0a9c3a90cd844a92540a62a869
holms <anna61c9@edu.zealand.dk>
2022-12-16 21:19:22 +0100

    Rettelser.

--- index.php
+++ index.php
@@ -13,10 +13,17 @@
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
+    <meta name=”description" content='En økoglogisk går, der går op i dyrevelfærd og frsike råvarer.
+          Gården har eget mejeri og driver en webshop med hjemmelavet produkter'>
+
+    <meta name=”robots" content=”index, follow">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="keywords"
+          content="søgeord1, søgeord2, søgeord3">
+
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 
