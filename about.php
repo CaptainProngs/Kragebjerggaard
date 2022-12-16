8022dd3b1c50cf7b2baebc237b3983dc93e9e1d0
Charlie <camille@camillekirstine.com>
2022-12-16 16:55:59 +0100

    added graphics for about page

--- about.php
+++ about.php
@@ -11,6 +11,7 @@
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,6 +20,8 @@
 <body>
 
 <?php include "nav.php"?>
+
+
 
 
 <?php include "footer.php" ?>
