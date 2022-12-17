21191dc391ea04b3d9c158dd285500f55c1cc42f
Charlie <camille@camillekirstine.com>
2022-12-17 22:32:40 +0100

    Added alt attributes to images on about page

--- about.php
+++ about.php
@@ -27,14 +27,14 @@
 
 <section class="aboutus">
     <div class="aboutus-img-cow" >
-        <img class="about-cow-img" src="images/cutecowaboutus.png">
+        <img class="about-cow-img" alt="cute cow" src="images/cutecowaboutus.png">
 
         <div class="text-end aboutus-title">
            <p>Om os</p>
         </div>
 
         <div class="aboutus-img-footer" >
-            <img src="images/footergraphicwhitePC.png">
+            <img alt="same graphic design as the footer in white" src="images/footergraphicwhitePC.png">
         </div>
     </div>
 </section>
@@ -45,25 +45,25 @@
             <div class="row mx-auto about-graphic">
 
                 <div class="col-6 col-lg-3 py-5 about-card1">
-                    <a href="#cowgraphic"><img class="mx-auto" src="images/cowgraphic.png"></a>
+                    <a href="#cowgraphic"><img alt="graphic of a cow in a field" class="mx-auto" src="images/cowgraphic.png"></a>
                     <h5>Friluftsdyr</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lectus quis sem lacinia nonummy. </p>
                 </div>
 
                 <div class="col-6 col-lg-3 py-5 about-card2">
-                    <a href="#shopgraphic"><img class="mx-auto" src="images/shopgraphic.png"></a>
+                    <a href="#shopgraphic"><img class="mx-auto" alt="Graphic of a tiny shop" src="images/shopgraphic.png"></a>
                     <h5>Gårdbutik</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lectus quis sem lacinia nonummy. </p>
                 </div>
 
                 <div class="col-6 col-lg-3 py-5 about-card3">
-                    <a href="#naturegraphic"><img class="mx-auto" src="images/leafgraphic.png"></a>
+                    <a href="#naturegraphic"><img class="mx-auto" alt="Graphic of leaves" src="images/leafgraphic.png"></a>
                     <h5>Natur</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lectus quis sem lacinia nonummy. </p>
                 </div>
 
                 <div class="col-6 col-lg-3 py-5 about-card4">
-                    <a href="#tractorgraphic"><img class="mx-auto" src="images/tractorgraphic.png"></a>
+                    <a href="#tractorgraphic"><img class="mx-auto" alt="Graphic of a tractor" src="images/tractorgraphic.png"></a>
                     <h5>Redskaber</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lectus quis sem lacinia nonummy. </p>
                 </div>
@@ -75,11 +75,11 @@
         <div class="container">
             <div class="row mb-5">
                 <div class="col-12 col-lg-6">
-                    <img class="about-farmhouse-img pb-4" src="images/farmhouseimage.png">
+                    <img class="about-farmhouse-img pb-4" alt="Farmhouse" src="images/farmhouseimage.png">
                     <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate</p>
                 </div>
                 <div class="col-12 col-lg-6">
-                    <img class="about-aboutus-img pb-4" src="images/aboutusimage.png">
+                    <img class="about-aboutus-img pb-4" alt="Couple holding herbs" src="images/aboutusimage.png">
                     <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate</p>
                 </div>
             </div>
@@ -90,7 +90,7 @@
         <div class="container-fluid bg-lightGreen section-top">
            <div class="row section-top-title">
                <div class="col">
-                   <a id="cowgraphic"><img class="w-25 mx-auto" src="images/cowgraphic.png"></a>
+                   <a id="cowgraphic"><img class="w-25 mx-auto" alt="Graphic of a cow" src="images/cowgraphic.png"></a>
                 <h1 class="text-center pb-4">Friluftsdyr</h1>
                </div>
            </div>
@@ -118,7 +118,7 @@
                     </div>
 
                     <div class="col-12 col-lg-6">
-                        <img class="chewingcow mx-auto mb-4" src="images/aboutchewingcow.png">
+                        <img class="chewingcow mx-auto mb-4" alt="Close up of a cow eating grass" src="images/aboutchewingcow.png">
                     </div>
                 </div>
             </div>
@@ -129,7 +129,7 @@
         <div class="container-fluid bg-dustyGrey section-top">
             <div class="row section-top-title">
                 <div class="col">
-                    <a id="shopgraphic"></a> <img class="w-25 mx-auto" src="images/shopgraphic.png"></a>
+                    <a id="shopgraphic"><img class="w-25 mx-auto" alt="Graphic of shop" src="images/shopgraphic.png"></a>
                     <h1 class="text-center pb-4">Gårdbutik</h1>
                 </div>
             </div>
@@ -157,7 +157,7 @@
                     </div>
 
                     <div class="col-12 col-lg-6">
-                        <img class="farmshop mx-auto mb-4" src="images/farmshopaboutimg.png">
+                        <img class="farmshop mx-auto mb-4" alt="Picture of the inside of a shop" src="images/farmshopaboutimg.png">
                     </div>
                 </div>
             </div>
@@ -168,14 +168,14 @@
         <div class="container-fluid bg-dustyGreen section-top">
             <div class="row section-top-title">
                 <div class="col">
-                    <a id="naturegraphic"><img class="w-25 mx-auto" src="images/leafgraphic.png"></a>
+                    <a id="naturegraphic"><img class="w-25 mx-auto" alt="Graphic of leaves" src="images/leafgraphic.png"></a>
                     <h1 class="text-center pb-4">Natur</h1>
                 </div>
             </div>
             <div class="container">
                 <div class="row mt-5">
                     <div class="col-12">
-                        <img class="natureabout mx-auto mb-4" src="images/aboutnatureimg.png">
+                        <img class="natureabout mx-auto mb-4" alt="Picture of a field" src="images/aboutnatureimg.png">
                     </div>
                     <div class="col-12 mb-5">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -199,7 +199,7 @@
         <div class="container-fluid bg-lightBlue section-top">
             <div class="row section-top-title">
                 <div class="col">
-                    <a id="tractorgraphic"><img class="w-25 mx-auto" src="images/tractorgraphic.png"></a>
+                    <a id="tractorgraphic"><img class="w-25 mx-auto" alt="Graphic of tractor" src="images/tractorgraphic.png"></a>
                     <h1 class="text-center pb-4">Redskaber</h1>
                 </div>
             </div>
@@ -213,7 +213,7 @@
                         </p>
                     </div>
                     <div class="col-12">
-                        <img class="natureabout mx-auto mb-4" src="images/abouttractor1img.png">
+                        <img class="natureabout mx-auto mb-4" alt="Picture of a tractor in a field" src="images/abouttractor1img.png">
                     </div>
                 </div>
             </div>
