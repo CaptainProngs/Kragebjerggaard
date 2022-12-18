e70a531b17495e703bacead9878afd1b7889f3e4
Charlie <camille@camillekirstine.com>
2022-12-18 16:15:20 +0100

    productinfo page finished

--- productinfo.php
+++ productinfo.php
@@ -1,4 +1,11 @@
 
+<?php
+require "settings/init.php";
+
+$id = $_GET['prodId'];
+$produkt = $db->sql("SELECT * FROM produkter WHERE prodId = :prodId", [':prodId' => $id]);
+?>
+
 <!DOCTYPE html>
 <html lang="da" xmlns="http://www.w3.org/1999/html">
 <head>
@@ -33,93 +40,92 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <link href="https://fonts.cdnfonts.com/css/sans-mateo" rel="stylesheet">
     <link rel="stylesheet" href="https://use.typekit.net/pgq7ydv.css">
-    <script src="https://cdn.tiny.cloud/1/ys9ivv1x233eg2mrq0d4prcpenkssm67kjeagomz2eix9ubu/tinymce/6/tinymce.min.js"
-            referrerpolicy="origin"></script>
+    <script src="https://cdn.tiny.cloud/1/ys9ivv1x233eg2mrq0d4prcpenkssm67kjeagomz2eix9ubu/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
 
-<?php
-require "settings/init.php";
-
-$id = $_GET['prodId'];
-$produkt = $db->sql("SELECT * FROM produkter WHERE prodId = :prodId", [':prodId' => $id]);
-?>
-
-<?php
-$produkter = $db->sql("SELECT * FROM produkter" );
-?>
-
-<?php
-$randomProdukter = array_rand($produkter);
-$randomProdukt = $produkter[$randomProdukter];
-
-$randomProdukter1 = array_rand($produkter);
-$randomProdukt1 = $produkter[$randomProdukter1];
-
-$randomProdukter2 = array_rand($produkter);
-$randomProdukt2 = $produkter[$randomProdukter2];
-?>
-
 <body>
+<?php include "nav.php"?>
+<picture>
+    <div>
+        <img src="images/productinfohero.png">
+    </div>
+</picture>
 
-<?php include "nav.php" ?>
-<picture><img alt="Billede af grøntsager set oppefra" src="images/productinfohero.png"></picture>
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <picture>
+                    <img class="prodbilledeinfo mt-4" src="images/<?php echo $produkt[0]->prodBilled; ?>">
+                </picture>
+            </div>
+            <div class="col-12 col-lg-6 mt-4 p-5">
+                <h1><?php echo $produkt[0]->prodNavn; ?></h1><br>
+                <div class="row">
+                    <div class="col-6 col-lg-6">
+                        <h5>Produktkategori</h5>
+                        <p><?php echo $produkt[0]->prodKategori; ?></p>
+                        <h1 class="pt-3"><?php echo $produkt[0]->prodPris; ?> DKK</h1>
+                    </div>
+                    <div class="col-6 col-lg-6">
+                        <h5>Produktbeskrivelse</h5>
+                        <p><?php echo $produkt[0]->prodBeskrivelse; ?></p>
+                    </div>
+                </div>
 
+                <div class="row mt-5">
+                    <div class="col-6 col-lg-6">
+                        <h5>Varianter</h5>
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Varianter</option>
+                            <option value="1">Variant 1</option>
+                            <option value="2">Variant 2</option>
+                            <option value="3">Variant 3</option>
+                        </select>
+                    </div>
 
+                    <div class="col-6 col-lg-6">
+                        <h5>Antal</h5>
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Antal</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="row mt-4">
+                    <div class="col">
+                        <div class="d-grid"><a href="#" class="btn" style="background-color: #71783a">Tilføj til kurv</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<div class="container my-5 mx-auto">
     <div class="row">
-        <div class="col-12 col-lg-6"><img alt="Billede af valgte produkt" src="images/<?php echo $produkt[0]->prodBilled; ?>" </div>
-        <div class="col-12 col-lg-6">
-            <h1><?php echo $produkt[0]->prodNavn; ?></h1>
+        <div class="col-4 col-lg-4 pt-3">
+            <div id="linefigure"></div>
         </div>
-    </div>
-
-<div class="row mx-auto">
-    <div class="card bg-cardPrimary col-12 col-lg-4 m-5" style="max-width: 18rem;">
-        <img src="images/<?php echo $randomProdukt->prodBilled; ?>" class="card-img-top" alt="...">
-        <div class="card-body mx-2 mb-2">
-            <h2 class="card-title p-0 mb-0"><?php echo $randomProdukt->prodNavn; ?></h2>
-            <p class="card-text"><?php echo $randomProdukt->prodBeskrivelse; ?> </p>
-
-            <div class="row mt-2">
-                <div class="col-10 pris"><p><?php ?></p></div>
-                <a class="col-2 mb-4" href=""><img src="images/darkCart.png" class="img-cart" alt="cart"></a>
-            </div>
-            <div class="d-grid"><a href="productinfo.php?prodId=${item.prodId}" class="btn" style="background-color: #71783a">Læs mere</a></div>
+        <div class="col-4 col-lg-4">
+            <h2 class="text-center py-auto">Andre købte også</h2>
         </div>
-    </div>
-
-    <div class="card bg-cardPrimary col-12 col-lg-4 m-5" style="max-width: 18rem;">
-        <img src="images/<?php echo $randomProdukt1->prodBilled; ?>" class="card-img-top" alt="...">
-        <div class="card-body mx-2 mb-2">
-            <h2 class="card-title p-0 mb-0"><?php echo $randomProdukt1->prodNavn; ?></h2>
-            <p class="card-text"><?php echo $randomProdukt1->prodBeskrivelse; ?> </p>
-
-            <div class="row mt-2">
-                <div class="col-10 pris"><p><?php ?></p></div>
-                <a class="col-2 mb-4" href=""><img src="images/darkCart.png" class="img-cart" alt="cart"></a>
-            </div>
-            <div class="d-grid"><a href="productinfo.php?prodId=${item.prodId}" class="btn" style="background-color: #71783a">Læs mere</a></div>
-        </div>
-    </div>
-
-    <div class="card bg-cardPrimary col-12 col-lg-4 m-5" style="max-width: 18rem;">
-        <img src="images/<?php echo $randomProdukt2->prodBilled; ?>" class="card-img-top" alt="...">
-        <div class="card-body mx-2 mb-2">
-            <h2 class="card-title p-0 mb-0"><?php echo $randomProdukt2->prodNavn; ?></h2>
-            <p class="card-text"><?php echo $randomProdukt2->prodBeskrivelse; ?> </p>
-
-            <div class="row mt-2">
-                <div class="col-10 pris"><p><?php ?></p></div>
-                <a class="col-2 mb-4" href=""><img src="images/darkCart.png" class="img-cart" alt="cart"></a>
-            </div>
-            <div class="d-grid"><a href="productinfo.php?prodId=${item.prodId}" class="btn" style="background-color: #71783a">Læs mere</a></div>
+        <div class="col-4 col-lg-4 pt-3">
+            <div id="linefigure"></div>
         </div>
     </div>
 </div>
 
 
+<div class="container mx-auto my-5">
+    <?php include "randomizedcards.php"?>
+</div>
+
 
 <?php include "footer.php"?>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-
 </body>
 </html>
