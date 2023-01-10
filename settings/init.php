d38a07e15d67a741ce2adab7277e4d6820a85a7e
holms <anna61c9@edu.zealand.dk>
2023-01-10 10:52:16 +0100

    server.

--- settings/init.php
+++ settings/init.php
@@ -3,16 +3,16 @@ require "classes/classDB.php";
 
 define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
-if(CONFIG_LIVE == 0){
+if(CONFIG_LIVE == 1){
     $DB_SERVER = "localhost";
     $DB_NAME = "krage";
     $DB_USER = "root";
     $DB_PASS = "";
 }else{
-    $DB_SERVER = "camillekirstine.com.mysql";
-    $DB_NAME = "camillekirstine_comkrage";
-    $DB_USER = "camillekirstine_comkrage";
-    $DB_PASS = "krage123";
+    $DB_SERVER = "annaholmstroem.dk.mysql";
+    $DB_NAME = "annaholmstroem_dkanna";
+    $DB_USER = "annaholmstroem_dkanna";
+    $DB_PASS = "root1234";
 }
 
 $db = new db($DB_SERVER, $DB_NAME, $DB_USER, $DB_PASS);
